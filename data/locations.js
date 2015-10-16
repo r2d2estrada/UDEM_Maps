0682a8273f946bbca0e241a0ce30e32430de5f4a
r2d2estrada <arturoestrada@Undertaker.local>
2015-10-16 11:41:26 -0500

    arrayList object instead (for easier quering)

--- data/locations.js
+++ data/locations.js
@@ -1,50 +1,50 @@
 var ItemsObj = [{
-    "Id": "CIA",
-    "Name": "Centro de Información y Atención a Alumnos (Rectoría)",
-    "Abrev": "CIA",
-    "latX": "",
-    "latY": "",
-    "Specs": ""
-}, {
-    "Id": "Biblio",
-    "Name": "Biblioteca UDEM",
-    "Abrev": "",
-    "latX": "",
-    "latY": "",
-    "Specs": ""
-}, {
-    "Id": "CC",
-    "Name": "Centro de la Comunidad",
-    "Abrev": "",
-    "latX": "",
-    "latY": "",
-    "Specs": ""
-}, {
-    "Id": "Theater",
-    "Name": "Teatro UDEM",
-    "Abrev": "",
-    "latX": "",
-    "latY": "",
-    "Specs": ""
-}, {
-    "Id": "CGS",
-    "Name": "Centro Roberto Garza Sada",
-    "Abrev": "CGS",
-    "latX": "",
-    "latY": "",
-    "Specs": ""
-}, {
-    "Id": "Sports",
-    "Name": "Centro Deportivo",
-    "Abrev": "",
-    "latX": "",
-    "latY": "",
-    "Specs": ""
-}, {
-    "Id": "STBKS",
-    "Name" : "Star Bucks",
-    "Abrev": "",
-    "latX": "",
-    "latY": "",
-    "Specs": "Entre edificios 1, 2, 3 y 4"
+    0: {
+        "Id": "CIA",
+        "Name": "Centro de Información y Atención a Alumnos (Rectoría)",
+        "Abrev": "CIA",
+        "latX": "",
+        "latY": "",
+        "Specs": ""
+    },
+    1: {
+        "Id": "Biblio",
+        "Name": "Biblioteca UDEM",
+        "Abrev": "",
+        "latX": "",
+        "latY": "",
+        "Specs": ""
+    },
+    2: {
+        "Id": "CC",
+        "Name": "Centro de la Comunidad",
+        "Abrev": "",
+        "latX": "",
+        "latY": "",
+        "Specs": ""
+    },
+    3: {
+        "Id": "Theater",
+        "Name": "Teatro UDEM",
+        "Abrev": "",
+        "latX": "",
+        "latY": "",
+        "Specs": ""
+    },
+    4: {
+        "Id": "CGS",
+        "Name": "Centro Roberto Garza Sada",
+        "Abrev": "CGS",
+        "latX": "",
+        "latY": "",
+        "Specs": ""
+    },
+    5: {
+        "Id": "Sports",
+        "Name": "Centro Deportivo",
+        "Abrev": "",
+        "latX": "",
+        "latY": "",
+        "Specs": ""
+    }
 }];
