554a1565414b25dd8bf9cfee373a51007078f009
r2d2estrada <artestradardz.85@gmail.com>
2015-10-25 15:10:46 -0600

    ok

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	if (!navigator.online) {
         swal ({
-            title: "No hay conexión a internet datos",
+            title: "No hay conexión a internet o datos",
             text: "Para el funcionamiento de UDEM MAPS es necesario estar conectado a la red (3G, 4G, Edge etc) o estar conectado a una red de wifi. Asegúrese de estar conectad@ y reinicie la aplicación.",
             type: "error",
             confirmButtonText: "Entendido"
