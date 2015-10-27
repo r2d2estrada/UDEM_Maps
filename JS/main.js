7b83f3b850658544bfe4cf7023776dc51e59b275
r2d2estrada <artestradardz.85@gmail.com>
2015-10-27 10:36:13 -0600

    first funtial commit

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
+var selectedLocation;
+
 $(document).ready(function(){
-	if (!navigator.online) {
+    if (!navigator.onLine) {
         swal ({
             title: "No hay conexión a internet o datos",
             text: "Para el funcionamiento de UDEM MAPS es necesario estar conectado a la red (3G, 4G, Edge etc) o estar conectado a una red de wifi. Asegúrese de estar conectad@ y reinicie la aplicación.",
@@ -8,6 +10,7 @@ $(document).ready(function(){
         });
     } else {
         loadContents();
+        loadLocations();
     }
 });
 
@@ -17,5 +20,69 @@ function loadContents() {
 
 	$('#year').append(year);
 	$('#maps-year').append(year);
-    $('#map').attr('src', UDEM_LOC_MAP);
+
+    loadMapLocation();
+}
+
+function loadMapLocation() {
+    //set to default loaction when initializing the application
+    if (selectedLocation == 'undefined' || selectedLocation == null) {
+        selectedLocation = defaultOBJ;
+    }
+
+    var position = selectedLocation.coords.split(',');
+    var posX = parseFloat(position[0]);
+    var posY = parseFloat(position[1]);
+
+    //set coordenates location
+    $('#map_canvas').gmap({ 'center': selectedLocation.coords, 'scrollwheel':false});
+    $('#map_canvas').gmap('option', 'mapTypeId', google.maps.MapTypeId.MAP);
+    $('#map_canvas').gmap('option', 'zoom', 16);
+
+    //set markers
+    $('#map_canvas').gmap('clear', 'markers'); //clear previous (if any)
+    $('#map_canvas').gmap('addMarker', { 'position': new google.maps.LatLng(posX, posY) }); //set new
+    $('#map_canvas').gmap('refresh');
+
+    //set labels
+    $('#titleToAppend').text(selectedLocation.Name);
+    $('#placeToDescribe').text(selectedLocation.Specs);
+}
+
+function loadLocations() {
+    $.each(ItemsObj, function(index, elem) {
+        var nodeToAppend = '<tr onclick="getRowValues(\'' + elem.Id + '\')">' +
+                                '<td>' + elem.Abrev + '</td>' +
+                                '<td>' +
+                                    elem.Name +
+                                '</td>' +
+                            '</tr>';
+
+        $('#locationsTBL tbody').append(nodeToAppend);
+    });
+
+    $('#locationsTBL').DataTable({
+        "paging":   false,
+        "info": false,
+        "bFilter": true,
+        "order": [[ 1, "asc" ]],
+        "columnDefs": [
+            { "targets": 0, "orderable": false },
+            { "targets": 1, "orderable": true}
+        ],
+        "oLanguage": { "sSearch": "" }
+    });
+}
+
+function getRowValues (id) {
+    $.each(ItemsObj, function(index, elem) {
+        if(elem.Id == id) {
+            selectedLocation = elem;
+            console.log(selectedLocation);
+            $('#locationsTBL_filter input').val('').trigger('search');
+            $('#appSideBar').removeClass('sidebar-open', '');
+        }
+    });
+
+    loadMapLocation();
 }
--- data/locations.js
+++ data/locations.js
@@ -1,52 +1,96 @@
-var ItemsObj = [{
-    0: {
+/**
+    {
+        "Id": "",
+        "Name": "",
+        "Abrev": "",
+        "coords": "",
+        "Specs": ""
+    }
+
+    Markup for images:
+    <img title='{Some-id}' src='icons/{fileName}.{extension}' />
+
+    Getting coordenates from google maps
+    1. Open Google maps
+    2. Type address (Universidad de monterrey)
+    3. Right click on spot > select what's here (¿Qué hay aquí?)
+    4. Copy coordinates
+    5. paste onbject attr value (coords)
+**/
+
+var ItemsObj = [
+    //default location
+    {
+        "Id": "JF",
+        "Name": "Jardín Fundadores",
+        "Abrev": "<img title='JF' src='icons/spotted1.png' />",
+        "coords": "25.660867, -100.419929",
+        "Specs": "Jardín central entre aulas, biblioteca y Centro de la Comunidad"
+    },
+    //default location
+    {
         "Id": "CIA",
         "Name": "Centro de Información y Atención a Alumnos (Rectoría)",
         "Abrev": "CIA",
-        "latX": "",
-        "latY": "",
-        "Specs": ""
+        "coords": "25.662682, -100.420060",
+        "Specs": "Edificio de Rectoría de la Universidad de Monterrey"
     },
-    1: {
+    {
         "Id": "Biblio",
         "Name": "Biblioteca UDEM",
-        "Abrev": "",
-        "latX": "",
-        "latY": "",
-        "Specs": ""
+        "Abrev": "<img title='SC' src='icons/books78.png'/>",
+        "coords": "25.661174, -100.419556",
+        "Specs": "Biblioteca de la Universidad de Monterrey"
     },
-    2: {
+    {
         "Id": "CC",
         "Name": "Centro de la Comunidad",
-        "Abrev": "",
-        "latX": "",
-        "latY": "",
-        "Specs": ""
+        "Abrev": "CCU",
+        "coords": "25.660323, -100.420082",
+        "Specs": "Abreviado CCU, En el e encuentra el teatro, la capilla, una sucursal de Banorte, cafetería con diversas opciones y aulas formativas"
     },
-    3: {
+    {
         "Id": "Theater",
         "Name": "Teatro UDEM",
-        "Abrev": "",
-        "latX": "",
-        "latY": "",
-        "Specs": ""
+        "Abrev": "<img title='Teatro-Udem' src='icons/theater3.png'/>",
+        "coords": "25.660313, -100.419621",
+        "Specs": "Se encuentra dentro del centro de la comunidad"
     },
-    4: {
+    {
         "Id": "CGS",
         "Name": "Centro Roberto Garza Sada",
         "Abrev": "CGS",
-        "latX": "",
-        "latY": "",
-        "Specs": ""
+        "coords": "25.661899, -100.422432",
+        "Specs": "Escuela de arte y arquitectura de la Universidad de Monterrey"
     },
-    5: {
+    {
         "Id": "Sports",
         "Name": "Centro Deportivo",
-        "Abrev": "",
-        "latX": "",
-        "latY": "",
-        "Specs": ""
+        "Abrev": "<img title='SC' src='icons/weightlifting4.png'/>",
+        "coords": "25.664104, -100.418194",
+        "Specs": "Ginmasio para uso de los alumnos"
+    },
+    {
+        "Id": "Stbks",
+        "Name": "Starbucks",
+        "Abrev": "<img title='Starbucks' src='icons/drink75.png'/>",
+        "coords": "25.661802, -100.420307",
+        "Specs": "Sucursal de Starbucks situado el pabellón central entre los edificios de aulas de clase"
+    },
+    {
+        "Id": "USP",
+        "Name": "Preparatoria UDEM Unidad San Pedro",
+        "Abrev": "USP",
+        "coords": "25.657528, -100.420972",
+        "Specs": "Preparatoria UDEM Campus San Pedro"
+    },
+    {
+        "Id": "Res",
+        "Name": "Residencias UDEM",
+        "Abrev": "<img title='res' src='icons/sleeping.png'/>",
+        "coords": "25.659879, -100.422199",
+        "Specs": "Residencias para estudiantes dentro del campus de la Universidad"
     }
-}];
+];
 
-var UDEM_LOC_MAP = "https://www.google.com/maps/embed?pb=!1m36!1m12!1m3!1d4155.367302523819!2d-100.42095202245028!3d25.661143667340507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m21!3e2!4m3!3m2!1d25.6620714!2d-100.4184868!4m5!1s0x8662bd5dcc519e6d%3A0x8d275c8a33ec1fb3!2sUniversidad+De+Monterrey+Avenida+Ignacio+Morones+Prieto+4500+Pte.!3m2!1d25.662837!2d-100.419921!4m5!1s0x0%3A0x0!2s25.661239%2C-100.419523!3m2!1d25.661239!2d-100.419523!4m3!3m2!1d25.6614906!2d-100.4204025!5e1!3m2!1ses!2smx!4v1443163334717";
+var defaultOBJ = ItemsObj[0];
